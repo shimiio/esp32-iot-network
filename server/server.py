3d6dc0991b7b88932005ac042d3ee85edd882a8e
Pasa <pavlo1191@gmail.com>
2026-08-08 01:18:24 +0200

    feat: add sqlite db

--- server/server.py
+++ server/server.py
@@ -1,5 +1,27 @@
 import json
+import sqlite3
+import time
 from http.server import BaseHTTPRequestHandler, HTTPServer
+
+DB_PATH = "sensors.db"
+LAST_WRITE_INTERVAL = 60  # sec
+_last_write = 0
+
+
+def init_db():
+    conn = sqlite3.connect(DB_PATH)
+    conn.execute("PRAGMA journal_mode=WAL;")
+    conn.execute("""
+            CREATE TABLE IF NOT EXISTS readings (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                ts REAL,
+                node TEXT,
+                temp REAL,
+                hum REAL
+            )
+        """)
+    conn.commit()
+    conn.close()
 
 
 class Handler(BaseHTTPRequestHandler):
@@ -8,10 +30,30 @@ class Handler(BaseHTTPRequestHandler):
             length = int(self.headers["Content-Length"])
             data = json.loads(self.rfile.read(length))
 
+            # display
             print("\033[H\033[J", end="")
-
             print(f"ESP2 - Temp: {data['esp2_temp']}°C | Hum: {data['esp2_hum']}%")
             print(f"ESP3 - Temp: {data['esp3_temp']}°C | Hum: {data['esp3_hum']}%")
+
+            global _last_write
+            now = time.time()
+
+            # write to the database
+            if now - _last_write >= LAST_WRITE_INTERVAL:
+                _last_write = now
+
+                conn = sqlite3.connect(DB_PATH)
+                conn.execute(
+                    "INSERT INTO readings (ts, node, temp, hum) VALUES (?, ?, ?, ?)",
+                    (now, "esp2", data["esp2_temp"], data["esp2_hum"]),
+                )
+
+                conn.execute(
+                    "INSERT INTO readings (ts, node, temp, hum) VALUES (?, ?, ?, ?)",
+                    (now, "esp3", data["esp3_temp"], data["esp3_hum"]),
+                )
+                conn.commit()
+                conn.close()
 
             self.send_response(200)
         except Exception as e:
@@ -24,4 +66,6 @@ class Handler(BaseHTTPRequestHandler):
         pass
 
 
-HTTPServer(("0.0.0.0", 8080), Handler).serve_forever()
+if __name__ == "__main__":
+    init_db()
+    HTTPServer(("0.0.0.0", 8080), Handler).serve_forever()
